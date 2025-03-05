43541be74215b50f4c99fa7eda1213ce4937fb6e
abasan01 <andrija3000@gmail.com>
2025-03-05 22:37:27 +0100

    Navbar + image grid

---
+++ vue/src/components/imageComponent.vue
@@ -0,0 +1,40 @@
+<script setup>
+const props = defineProps({
+    image: {
+        type: Object,
+        required: true,
+    },
+});
+console.log();
+</script>
+
+<template>
+    <div class="group relative">
+        <div
+            class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+            :style="{ backgroundImage: `url('${image.imageSrc}')` }"
+        >
+            <div class="mt-4 flex justify-between">
+                <div class="absolute bottom-0 left-0 right-0 p-4">
+                    <h3 class="text-sm text-gray-700">
+                        <a :href="image.href">
+                            <span aria-hidden="true" class="absolute inset-0" />
+                            {{ image.name }}
+                        </a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500">
+                        {{ image.author }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+div {
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+}
+</style>
--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,9 +1,209 @@
+<script setup>
+import imageButton from "../components/imageComponent.vue";
+
+const images = [
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 2,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/54/cb/54cb64c80f5be4cf1f2d60e3a8015546.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 1216,
+        height: 1680,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 3000,
+        height: 2000,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://cdn.donmai.us/720x720/3b/9c/3b9c82dbf7567d4473c9a49a1198b703.webp",
+        imageAlt: "Front of men's Basic Tee in black.",
+        author: "$35",
+        color: "Black",
+        width: 576,
+        height: 720,
+    },
+];
+const test = [1, 2, 3];
+</script>
+
 <template>
-    <h1>Dashboard</h1>
+    <div class="bg-white">
+        <div class="board-wrapper">
+            <div class="board masonry-board">
+                <div
+                    class="card image-card"
+                    v-for="(image, index) in images"
+                    :key="image.id"
+                    :style="{
+                        '--n': index + 1,
+                        '--img-ratio': image.width / image.height,
+                    }"
+                >
+                    <imageButton :image="image"> </imageButton>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script>
-export default {
-    name: "Dashboard",
-};
-</script>
+<style></style>
